672b3e4cbf30a92731cbd43e5f1d2e2ba7afd078
mgryszko <marcin.gryszko@lastminute.com>
2018-11-16 19:29:07 +0100

    Download access token to be used later in scripts

--- token.rb
+++ token.rb
@@ -1,12 +1,17 @@
 require 'sinatra'
 require 'oauth2'
 
-get '/auth' do
-  redirect client.auth_code.authorize_url({:redirect_uri => 'http://localhost:4567/onauth', :scope => :read})
+port = 8080
+
+set :port, port
+
+get '/' do
+  redirect client.auth_code.authorize_url({:redirect_uri => "http://localhost:#{port}/auth", :scope => :read})
 end
 
-get '/onauth' do
+get '/auth' do
   access_token = client.auth_code.get_token(request.params['code'])
+  File.open('.access-token', 'w') { |file| file.write(access_token.token) }
   access_token.token
 end
 
